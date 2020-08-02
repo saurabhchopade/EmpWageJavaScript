d54586dd6bfbc1c53d96c860e2cf10bea423aec9
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-02 12:41:48 +0530

    ADDED : Code to calculate daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,8 +1,16 @@
-var attendanceStatus = Math.floor(Math.random()*10%2);
-	if(attendanceStatus==1){
+//CONSTANTS
+var RATE_PER_HOUR = 20;
+var DAILY_HOURS = 8;
+//VARIABLES
+var salary = 0; 
+var attendanceStatus = Math.floor(Math.random()* 10 % 2);
+	if(attendanceStatus == 1){
 		console.log("Employee is Present");
+		salary = (salary = DAILY_HOURS * RATE_PER_HOUR);
+		console.log("Salary="+salary);
 	}
 	else{
 		console.log("Employee is Absent");
+		console.log("Salary="+ salary);
 	}
 
