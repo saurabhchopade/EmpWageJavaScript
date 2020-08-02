fffd6717860bafd9575866ab375263ad1b2b42eb
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-02 12:45:45 +0530

    REFACTORED : Code to declare variable as const

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,13 +1,13 @@
 //CONSTANTS
-var RATE_PER_HOUR = 20;
-var DAILY_HOURS = 8;
+const RATE_PER_HOUR = 20;
+const DAILY_HOURS = 8;
 //VARIABLES
 var salary = 0; 
 var attendanceStatus = Math.floor(Math.random()* 10 % 2);
 	if(attendanceStatus == 1){
 		console.log("Employee is Present");
 		salary = (salary = DAILY_HOURS * RATE_PER_HOUR);
-		console.log("Salary="+salary);
+		console.log("Salary="+ salary);
 	}
 	else{
 		console.log("Employee is Absent");
