a90f0af6ee3a24d10217be24b61f9c9c6ae5a418
saurabh chopade <saurabhchopade3250@gmail.com>
2020-08-02 13:18:50 +0530

    ADDED : Code for part time wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,16 +1,19 @@
 //CONSTANTS
 const RATE_PER_HOUR = 20;
-const DAILY_HOURS = 8;
+const  IS_PART_TIME = 2;
+const IS_FULL_TIME = 1;
 //VARIABLES
+var empHours = 0;
 var salary = 0; 
-var attendanceStatus = Math.floor(Math.random()* 10 % 2);
-	if(attendanceStatus == 1){
-		console.log("Employee is Present");
-		salary = (salary = DAILY_HOURS * RATE_PER_HOUR);
-		console.log("Salary="+ salary);
+var empCheck = Math.floor((Math.random()* 10) % 3);
+	if(empCheck == IS_FULL_TIME){
+		empHours = 8;
+	}
+	else if(empCheck = IS_PART_TIME){
+ 		empHours = 4;
 	}
 	else{
-		console.log("Employee is Absent");
-		console.log("Salary="+ salary);
+		 empHours = 0;
 	}
-
+salary = empHours * RATE_PER_HOUR;
+console.log("Salary="+ salary);
